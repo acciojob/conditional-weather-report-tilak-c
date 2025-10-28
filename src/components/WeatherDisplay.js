e8bdabe9408f796039ade5d2f031e3f9f81a57ac
Tilak <adireddytilak@gmail.com>
2025-10-28 15:13:40 +0530

    Completed Assignment

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -4,7 +4,7 @@ const WeatherDisplay = (data) => {
     const {temperature,conditions}=data;
     return (
     <>
-    {temperature>20?<p style={{color:"red"}}>temperature</p>:<p style={{color:"blue"}}>temperature</p>}
+    {temperature>20?<p><span style={{color:"red"}}>temperature</span></p>:<p><span style={{color:"blue"}}>temperature</span></p>}
     <p>{conditions}</p>
     </>
   )
